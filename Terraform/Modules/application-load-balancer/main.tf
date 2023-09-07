62b978ae5e8f3c432f676733c90ca790a3c6f480
digitalden3 <denizyilmaz@hotmail.co.uk>
2023-09-07 21:30:23 +0100

    created alb module

---
+++ Terraform/Modules/application-load-balancer/main.tf
@@ -0,0 +1,67 @@
+# create application load balancer
+resource "aws_lb" "application_load_balancer" {
+  name               = "${var.project_name}-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group_id]
+  subnets            = [var.public_subnet_az1_id, var.public_subnet_az2_id]
+  enable_deletion_protection = false
+
+  tags   = {
+    Name = "${var.project_name}-ALB"
+  }
+}
+
+# create target group
+resource "aws_lb_target_group" "alb_target_group" {
+  name        = "${var.project_name}-TG"
+  target_type = "ip"
+  port        = "80"
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 300
+    path                = "/"
+    timeout             = 60
+    matcher             = 200
+    healthy_threshold   = 5
+    unhealthy_threshold = 5
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# create a listener on port 80 with redirect action
+resource "aws_lb_listener" "alb_http_listener" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = 443
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# create a listener on port 443 with forward action
+resource "aws_lb_listener" "alb_https_listener" {
+  load_balancer_arn  = aws_lb.application_load_balancer.arn
+  port               = 443
+  protocol           = "HTTPS"
+  ssl_policy         = "ELBSecurityPolicy-2016-08"
+  certificate_arn    = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = var.aws_lb_target_group.alb_target_group.arn
+  }
+}
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,7 +16,7 @@ module vpc {
   private_subnet_az2_cidr     = var.private_subnet_az2_cidr
 }
 
-# create nat-gateay
+# create nat-gateway
 module nat-gateway {
   source                      = "./modules/nat-gateway"
   public_subnet_az1_id        = module.vpc.public_subnet_az1_id
@@ -25,4 +25,15 @@ module nat-gateway {
   vpc_id                      = module.vpc.vpc_id
   private_subnet_az1_id       = module.vpc.private_subnet_az1_id
   private_subnet_az2_id       = module.vpc.private_subnet_az2_id
+}
+
+# create application load balancer
+module application_load_balancer {
+  source                      = "./modules/application_load_balancer"
+  project_name                = module.vpc.project_name
+  alb_security_group_id       = module.security_group.alb_security_group_id 
+  public_subnet_az1_id        = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id        = module.vpc.public_subnet_az2_id
+  vpc_id                      = module.vpc.vpc_id
+  certificate_arn             = module.acm.certificate_arn
 }
